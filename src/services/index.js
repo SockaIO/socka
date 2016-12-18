dd09cffb390bd3e24605b0a8a0f3209e8925160b
Remi Robert <th3o.smith@gmail.com>
2016-12-18 14:32:36 +0100

    Add songParser and fileManager to the service index

--- src/services/index.js
+++ src/services/index.js
@@ -6,11 +6,15 @@ import * as theme from './theme';
 import * as judge from './judge';
 import * as input from './input';
 import * as player from './player';
+import * as fileManager from './fileManager';
+
+export {default as songParser} from './songParser';
 
 export {
   theme,
   judge,
   input,
-  player
+  player,
+  fileManager
 };
 
