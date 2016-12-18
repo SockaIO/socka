1b1050ba20d6569e9635e5d7e75356b20a148692
Remi Robert <th3o.smith@gmail.com>
2016-12-18 14:28:01 +0100

    Player Service

--- src/services/index.js
+++ src/services/index.js
@@ -2,15 +2,15 @@
  * @namespace services
  */
 
-//export { default as Player } from './player'
-
 import * as theme from './theme';
 import * as judge from './judge';
 import * as input from './input';
+import * as player from './player';
 
 export {
   theme,
   judge,
-  input
+  input,
+  player
 };
 
---
+++ src/services/player.js
@@ -0,0 +1,54 @@
+'use strict';
+
+
+let players = new Set();
+
+export {
+  CreatePlayer,
+  GetPlayers
+};
+
+/**
+ * Player entity.
+ * Store the configuration and controller
+ */
+class Player {
+
+  /**
+   * Craete a player with empty configuration
+   * and no controller
+   * @param {String} name - Player name
+   */
+  constructor() {
+    this.configuration = new Map();
+    this.controller = null;
+    this.name = 'Player ' + (Player.Players.size + 1);
+  }
+
+  /**
+   * Controller setter
+   * @param {Mapping} mapping - Mapping used by the player
+   */
+  setMapping(mapping) {
+    this.mapping = mapping;
+  }
+}
+
+/**
+ * Create a new Player
+ * @return {Player} New Player
+ */
+function CreatePlayer() {
+  const p = new Player();
+  players.add(p);
+  return p;
+}
+
+/**
+ * Get the Players
+ * @returns {Player|Iterator} Iterator over the players
+ */
+function GetPlayers() {
+  return players.values();
+}
+
