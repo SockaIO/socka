474d2867b4419fa33479ea055bff6852061086f5
Remi ROBERT <remi.robert@larez.fr>
2016-12-28 20:07:39 +0100

    Coherence in constant exports

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,11 +2,13 @@
  * @namespace constants
  */
 
+import * as chartC from './chart';
 import * as judgeC from './judge';
 import * as inputC from './input';
 import * as resourcesC from './resources';
 
 export {
+  chartC,
   judgeC,
   inputC,
   resourcesC
